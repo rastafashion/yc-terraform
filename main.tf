0cd8d8198c80f86e9fb519c367c57416bb8f9062
rastafashion <dnd1@yandex.ru>
2022-05-18 13:08:31 +0000

    minor changes

--- main.tf
+++ main.tf
@@ -8,31 +8,31 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "AQAAAAAAqbiyAATuwbMt3LWvPETCvkcK7oIWQTo"
-  cloud_id  = "b1gdv9ii6m1jhh3058dk"
-  folder_id = "b1ghk6ucpjct1q8ee8nr"
+  token     = "Яндекс OAuth token"
+  cloud_id  = "id_облака"
+  folder_id = "id_папки"
   zone      = "ru-central1-a"
 }
 
-resource "yandex_compute_instance" "vm-1" {
-  name = "terraform3"
-  platform_id = "standard-v3" 
+resource "yandex_compute_instance" "some_node" {
+  name = "some_name" #полезно давать простое уникальное значение, чтобы потом использовать его в yandex CLI
+  platform_id = "standard-v3" #наиболее бюджетная конфигурация на базе Intel Ice Lake
 
   resources {
-    cores  = 2
-    memory = 4
+    cores  = 2 #количество ядер
+    memory = 4 #объем памяти GB
   }
 
   boot_disk {
     initialize_params {
-      type     = "network-hdd"
-      size     = 40
-      image_id = "fd82re2tpfl4chaupeuf"
+      type     = "network-hdd" #можно изменить на "network-ssd", будет быстрее, но дороже
+      size     = 40 #размер загрузочного дика GB. 
+      image_id = "fd82re2tpfl4chaupeuf" #установочный образ ОС. В данном примере CentOS 7
     }
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
+    subnet_id = yandex_vpc_subnet.subnet-1.id 
     nat       = true
   }
   
@@ -42,19 +42,19 @@ resource "yandex_compute_instance" "vm-1" {
 
 }
 resource "yandex_vpc_network" "network-1" {
-  name = "network1"
+  name = "network1" #название сети. оставить как есть либо заменить на свой вариант во всем документе
 }
 
 resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
+  name           = "subnet1" #название подсети. оставить как есть либо заменить на свой вариант во всем документе
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
+  v4_cidr_blocks = ["192.168.10.0/24"] #пулл серых адресов для создаваемой подсети
 }
 
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+output "internal_ip_address_some_node" {
+  value = yandex_compute_instance.some_node.network_interface.0.ip_address
 }
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+output "external_ip_address_some_node" {
+  value = yandex_compute_instance.some_node.network_interface.0.nat_ip_address
 }
